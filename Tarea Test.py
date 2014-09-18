65cc103848c3501579df6422e458f5aff2e05647
Grandpababy <abraham_1396@hotmail.com>
2014-09-18 12:49:47 -0600

    Tarea Algoritmos

---
+++ Tarea Test.py
@@ -0,0 +1,49 @@
+x=True
+print (x)
+x= not (x)
+print (x)
+x=4
+print(x)
+x += 4
+print (x)
+x+=2.25
+print (x)
+x= round(x)
+y= (x)
+print (x)
+x=x/1.15
+print (x)
+x= y//1.15
+print (x)
+x= not (x)
+x= 5%2
+print (x)
+x=4/2
+print (x)
+x= 4//3
+print(x)
+x=9845**24
+print (x)
+x="Hola variable"
+print (x)
+x= (x)+ " tipo cadena"
+print (x)
+
+r=10
+pi=3.1416
+x= (pi)*((r)**2)
+x= round(x,4)
+print(x)
+
+c=12
+gradf= ((c)*9/5)+32
+x= (gradf)
+x= int(x)
+print (x)
+
+peso=72
+alt=1.85
+mc=((peso)/((alt)**2))
+mc=round(mc,10)
+print(mc)
+
