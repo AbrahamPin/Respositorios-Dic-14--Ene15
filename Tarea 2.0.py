be16100bf78b3d2f8be5b8538e30a5d0a9a001bd
Grandpababy <abraham_1396@hotmail.com>
2014-09-18 12:49:40 -0600

    Tarea Funciones

---
+++ Tarea 2.0.py
@@ -0,0 +1,87 @@
+#1
+def Cuadrado(numero):
+    return numero **2
+numero= input ("Teclee el numero que quieres elevado al cuadrado: ")
+resultado= (Cuadrado(int(numero)))
+print("El resultado es: {}".format(resultado))
+#2
+import datetime
+def Cumple(edad):
+    actual= datetime.datetime.now().year
+    dedad= datetime.datetime.strptime(dfecha, "%d/%m/%Y").year
+    return (actual-dedad)
+dfecha= input("Cual es tu fecha de nacimiento? (formato dd/mm/aaaa) ")
+edad= Cumple (dfecha)
+print ("Tienes {} años".format(edad))
+#3
+import datetime
+def Viernes(fecha):
+    fecha= datetime.datetime.strptime(dfecha, "%d/%m/%Y")
+    if (fecha.weekday() == 4):
+        print (True)
+    else:
+        print (False)
+    return fecha
+dfecha= input("Que fecha es hoy? (formato dd/mm/aaaa): ")
+cfecha= Viernes(dfecha)
+#4
+import datetime
+def Años(fecha):
+    fecha2= (datetime.date(fecha.year,fecha.month,fecha.day))
+    fecha3= datetime.date(fecha2.year+10, fecha2.month, fecha2.day)
+    return ("{:%d (%A) %m %Y}".format(fecha3))
+cfecha=input("Cual es la fecha de hoy? (formato dd/mm/aaaa) ")
+dfecha=datetime.datetime.strptime(cfecha, "%d/%m/%Y")
+print (Años(dfecha))
+#5
+def imc(peso,estatura):
+    return (peso/(estatura^2))
+    
+peso= input("Cuanto pesas (en kgs)? ")
+estatura= (float(input("Cuanto mides en mts)? ")))
+peso= int(peso)
+estatura= int(estatura)
+IMC= (imc(peso, estatura))
+print ("Tu IMC es: {}".format(IMC))
+#6
+import math
+def AreaCirculo(radio):
+    return math.pi * (radio**2)
+radio = input ("Teclee el radio del circulo ")
+area=(AreaCirculo(int(radio)))
+print("El area del circulo es: {}".format(round(area,3)))
+#7
+def division(x):
+    y= x/2
+    
+    if (y ==int (y)):
+        return True
+    else:
+        return False
+print(division(5))
+print(division(4))
+#8
+def division(x):
+    y= x/3
+    
+    if (y ==int (y)):
+        return True
+    else:
+        return False
+print(division(8))
+print(division(9))
+#9
+def division(x):
+    y= x/3
+    z= x/2
+    if (((y) ==int (y)) and ((z)==int(z))):
+        return True
+    else:
+        return False
+print(division(12))
+print(division(9))
+#10
+def lista(l):
+    return (sum(l)/len(l))
+l= [1,5,1,2,7,5,4,7,1,4]
+print (lista(l))
