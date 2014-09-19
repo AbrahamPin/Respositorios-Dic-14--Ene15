fc238cd1dcd38433580a7900e3299ef2579d86f0
Grandpababy <abraham_1396@hotmail.com>
2014-09-18 22:54:22 -0600

    Update Tarea 2.0.py

--- Tarea 2.0.py
+++ Tarea 2.0.py
@@ -1,6 +1,7 @@
 #1
 def Cuadrado(numero):
     return numero **2
+print ("Ejercicio #1")
 numero= input ("Teclee el numero que quieres elevado al cuadrado: ")
 resultado= (Cuadrado(int(numero)))
 print("El resultado es: {}".format(resultado))
@@ -10,6 +11,7 @@ def Cumple(edad):
     actual= datetime.datetime.now().year
     dedad= datetime.datetime.strptime(dfecha, "%d/%m/%Y").year
     return (actual-dedad)
+print ("Ejercicio #2")
 dfecha= input("Cual es tu fecha de nacimiento? (formato dd/mm/aaaa) ")
 edad= Cumple (dfecha)
 print ("Tienes {} años".format(edad))
@@ -22,6 +24,7 @@ def Viernes(fecha):
     else:
         print (False)
     return fecha
+print ("Ejercicio #3")
 dfecha= input("Que fecha es hoy? (formato dd/mm/aaaa): ")
 cfecha= Viernes(dfecha)
 #4
@@ -30,13 +33,14 @@ def Años(fecha):
     fecha2= (datetime.date(fecha.year,fecha.month,fecha.day))
     fecha3= datetime.date(fecha2.year+10, fecha2.month, fecha2.day)
     return ("{:%d (%A) %m %Y}".format(fecha3))
+print ("Ejercicio #4")
 cfecha=input("Cual es la fecha de hoy? (formato dd/mm/aaaa) ")
 dfecha=datetime.datetime.strptime(cfecha, "%d/%m/%Y")
 print (Años(dfecha))
 #5
 def imc(peso,estatura):
     return (peso/(estatura^2))
-    
+print ("Ejercicio #5")    
 peso= input("Cuanto pesas (en kgs)? ")
 estatura= (float(input("Cuanto mides en mts)? ")))
 peso= int(peso)
@@ -47,6 +51,7 @@ print ("Tu IMC es: {}".format(IMC))
 import math
 def AreaCirculo(radio):
     return math.pi * (radio**2)
+print ("Ejercicio #6")
 radio = input ("Teclee el radio del circulo ")
 area=(AreaCirculo(int(radio)))
 print("El area del circulo es: {}".format(round(area,3)))
@@ -58,8 +63,11 @@ def division(x):
         return True
     else:
         return False
-print(division(5))
-print(division(4))
+print ("Ejercicio #7")
+print ("comprobacion con el 5")
+print (division(5))
+print ("comprobacion con el 4")
+print (division(4))
 #8
 def division(x):
     y= x/3
@@ -68,8 +76,11 @@ def division(x):
         return True
     else:
         return False
-print(division(8))
-print(division(9))
+print ("Ejercicio #8")    
+print ("comprobacion con el 8")
+print (division(8))
+print ("comprobacion con el 9")
+print (division(9))
 #9
 def division(x):
     y= x/3
@@ -78,10 +89,15 @@ def division(x):
         return True
     else:
         return False
-print(division(12))
-print(division(9))
+print ("Ejercicio #9")
+print ("comprobacion con el 12")
+print (division(12))
+print ("comprobacion con el 9")
+print (division(9))
 #10
 def lista(l):
     return (sum(l)/len(l))
 l= [1,5,1,2,7,5,4,7,1,4]
-print (lista(l))
+print ("Ejercicio #10")
+print ("El promedio de los numeros [1,5,1,2,7,5,4,7,1,4] es de",(lista(l)))
+
